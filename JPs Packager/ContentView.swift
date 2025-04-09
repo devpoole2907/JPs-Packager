fc1d3d2d12cf2a4b7cf5ff1f7fd2a24cf25dab43
James Poole <jamesdpoole@icloud.com>
2025-04-09 16:27:56 +1200

    Initial Commit

---
+++ JPs Packager/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  JPs Packager
+//
+//  Created by James Poole on 09/04/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ JPs Packager/JPs_PackagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  JPs_PackagerApp.swift
+//  JPs Packager
+//
+//  Created by James Poole on 09/04/2025.
+//
+
+import SwiftUI
+
+@main
+struct JPs_PackagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
